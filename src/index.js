6aaa66978c9be388c643541a80c1bc613de43ca0
Ankit Garg <gargankit1061997@gmail.com>
2020-08-06 18:43:55 +0530

    Little Changes

--- src/index.js
+++ src/index.js
@@ -4,7 +4,7 @@ import ReactDOM from "react-dom";
 import { BrowserRouter as Router } from "react-router-dom";
 import App from "./App";
 import * as serviceWorker from "./serviceWorker";
-import { RoomProvider, RoomContext } from "./context";
+import { RoomProvider } from "./context";
 
 ReactDOM.render(
   <RoomProvider>
